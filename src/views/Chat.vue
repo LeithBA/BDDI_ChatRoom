f52c6372ce404e9ba2cde73a4b20b805eb3a883e
Leith B.A <leithba@rog.lan>
2020-09-16 22:48:55 +0200

    styling start

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <UsersList :users="store.users" />
-    <MessagesList :messages="store.messages" />
-    <MessageBox @sendMessage="onSendMessage" />
+  <div id="wrapper">
+    <UsersList :users="store.users" id="usersList" />
+    <div id="chatPanel">
+      <MessagesList :messages="store.messages" id="messagesList" />
+      <MessageBox @sendMessage="onSendMessage" id="messagesBox" />
+    </div>
   </div>
 </template>
 
@@ -32,3 +34,48 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+#wrapper {
+  display: grid;
+
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+  height: 100vh;
+  width: 100vh;
+}
+#chatPanel {
+  grid-column: 2 / 4;
+  grid-row: 1 / 3;
+}
+#usersList {
+  grid-row: 1 / 3;
+  height: 100vh;
+  width: 15vh;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #202020;
+}
+#messagesList {
+  height: 90vh;
+  overflow: scroll;
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #202020;
+}
+#messagesBox {
+  height: 10vh;
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #202020;
+}
+</style>
